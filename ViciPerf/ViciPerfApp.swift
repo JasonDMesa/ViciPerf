ed2512233ac7a63e0f016c77d10afadfd82001cd
JasonDMesa <jason.mesa023@myci.csuci.edu>
2024-03-07 22:59:43 -0800

    Initial Commit

---
+++ ViciPerf/ViciPerfApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViciPerfApp.swift
+//  ViciPerf
+//
+//  Created by Jason Mesa on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ViciPerfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
