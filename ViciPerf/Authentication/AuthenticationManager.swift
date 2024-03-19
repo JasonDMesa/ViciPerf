48c3eaf00c8fe87896ac48eff3abf9efa69c7449
JasonDMesa <jason.mesa023@myci.csuci.edu>
2024-03-19 11:45:33 -0700

    1st Push

---
+++ ViciPerf/Authentication/AuthenticationManager.swift
@@ -0,0 +1,85 @@
+//
+//  AuthenticationManager.swift
+//  ViciPerf
+//
+//  Created by Jason Mesa on 3/8/24.
+//
+
+import Foundation
+import FirebaseAuth
+
+struct AuthDataResultModel {
+    let uid: String
+    let email: String?
+    let photoUrl: String?
+
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email
+        self.photoUrl = user.photoURL?.absoluteString
+    }
+    
+}
+
+final class AuthenticationManager {
+    
+    static let shared = AuthenticationManager()
+    private init() { }
+    
+    //Need to generate a custom error
+    //Not thrown asyncronously which means it only checks locally for user
+    func getAuthenticatedUser() throws -> AuthDataResultModel {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return AuthDataResultModel(user: user)
+    }
+    
+        //async checks the database
+    @discardableResult
+    func createUser(email: String, password: String) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user) 
+        // how to not use "singleton" (Dependency Injection - SwiftUI Advanced #16 on youtube
+    }
+    
+    @discardableResult
+    func signInUser(email: String, password: String) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+    }
+    
+    func resetPassword(email: String) async throws {
+        try await Auth.auth().sendPasswordReset(withEmail: email)
+    }
+    
+    func updatePassword(password: String) async throws {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        
+       try await user.updatePassword(to: password)
+    }
+    
+    func updateEmail(email: String) async throws {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        
+       try await user.sendEmailVerification(beforeUpdatingEmail: email)
+    }
+    
+    //if does not throw an error, successfully signed out
+    func signOut() throws {
+       try Auth.auth().signOut()
+    }
+    
+    func delete() async throws {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badURL)
+        }
+        
+        try await user.delete()
+    }
+}
---
+++ ViciPerf/Authentication/RootView.swift
@@ -0,0 +1,34 @@
+//
+//  RootView.swift
+//  ViciPerf
+//
+//  Created by Jason Mesa on 3/11/24.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    
+    @State private var showSignInView: Bool = false
+    
+    var body: some View {
+        ZStack {
+            NavigationStack {
+                SettingsView(showSignInView: $showSignInView)
+            }
+        }
+        .onAppear {
+            let authUser = try? AuthenticationManager.shared.getAuthenticatedUser()
+            self.showSignInView = authUser == nil
+        }
+        .fullScreenCover(isPresented: $showSignInView) {
+            NavigationStack {
+                AuthenticationView(showSignInView: $showSignInView)
+            }
+        }
+    }
+}
+
+#Preview {
+    RootView()
+}
