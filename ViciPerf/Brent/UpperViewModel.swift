3eb3d808bd031cc6be7ead2c38bc89fac5fc4692
JasonDMesa <jason.mesa023@myci.csuci.edu>
2024-04-25 12:00:37 -0700

    Authentication Complete, home page needs navigation, tab bar needs layering

---
+++ ViciPerf/Brent/UpperViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UpperViewModel.swift
+//  ViciPerf
+//
+//  Created by Jason Mesa on 4/25/24.
+//
+
+import Foundation
---
+++ ViciPerf/Brent/VideoUploader.swift
@@ -0,0 +1,8 @@
+//
+//  VideoUploader.swift
+//  ViciPerf
+//
+//  Created by Jason Mesa on 4/25/24.
+//
+
+import Foundation
